d7fdd4865f058dcefaf520413c3372b431040ba4
Marc <mcalafellsmx@gmail.com>
2016-10-21 16:54:18 +0200

    second test

---
+++ tests/TestApiTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TestApiTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+      //  $this->assertTrue(true);
+        $this->json('GET', '/app/task')
+           // ->dump();
+            -> seeJson();
+    }
+}
