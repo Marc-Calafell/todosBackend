b9244771da82ad039998131ac9c3d1b1c669f6e5
Marc <mcalafellsmx@gmail.com>
2016-10-21 16:28:07 +0200

    created a resource controller

---
+++ database/seeds/TaskTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TaskTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Task::class,30)->create();
+        factory('App\Task')->make();
+    }
+
+
+}
