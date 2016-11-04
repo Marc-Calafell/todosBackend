a4821b0d73e7f72a23142a079b93e2fdbb8d1634
Marc <mcalafellsmx@gmail.com>
2016-11-04 17:30:28 +0100

    after artisan tinker

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['name','done','priority'];
+
+
+    /**
+     * @return array
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
+
