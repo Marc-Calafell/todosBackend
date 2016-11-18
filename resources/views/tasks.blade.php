68ef5d3febc684be148ac467a1fcd254e4ce8293
Marc <mcalafellsmx@gmail.com>
2016-11-18 17:50:55 +0100

    thinkering with components

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -9,17 +9,11 @@
 
 @section('main-content')
 
- <todos-list></todos-list>
+ <example></example>
+
+ <todos></todos>
 
 
 
-{{--<div id="app" xmlns:v-on="http://www.w3.org/1999/xhtml">--}}
-    {{--<p v-show="seen">@{{message}}</p>--}}
-    {{--<input type="text" v-model="message">--}}
-    {{--<button v-on:click="reverseMessage">Reverse</button>--}}
-    {{--<ol>--}}
-        {{--<li v-for="todo in todos">@{{todo.name}} | @{{todo.done}} | @{{todo.priority}}</li>--}}
-    {{--</ol>--}}
-{{--</div><script src="js/app.js"></script>--}}
 
 @endsection
