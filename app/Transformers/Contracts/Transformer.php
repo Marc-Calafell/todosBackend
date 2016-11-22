3135976f10fa6d3728c033e6b572c3fd06f9d762
Marc <mcalafellsmx@gmail.com>
2016-11-22 21:47:16 +0100

    protecting api

--- app/Transformers/Contracts/Transformer.php
+++ app/Transformers/Contracts/Transformer.php
@@ -13,10 +13,9 @@ interface Transformer
      * @return mixed
      */
     public function transform($resource);
-<<<<<<< HEAD
 
     public function transformCollection($resources);
+
+
 }
-=======
-}
->>>>>>> 77e35ab6dc972761f25ec180c66aba89ba0b8c2c
+
