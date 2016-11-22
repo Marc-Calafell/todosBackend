ccf671aad3aee53f638f4d1ff34b1a887824119c
Marc <mcalafellsmx@gmail.com>
2016-11-22 19:53:04 +0100

    modified seeder and factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,10 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name'           => $faker->name,
-        'email'          => $faker->unique()->safeEmail,
-        'password'       => $password ?: $password = bcrypt('secret'),
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'apitoken' => str_random(10),
         'remember_token' => str_random(10),
     ];
 });
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('apitoken',60)->unique();
             $table->rememberToken();
             $table->timestamps();
         });
