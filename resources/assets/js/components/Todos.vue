068e10570a97d8e934c9d33bdba41660cb3ff478
Marc <mcalafellsmx@gmail.com>
2016-11-29 16:05:06 +0100

    sync

--- resources/assets/js/components/Todos.vue
+++ resources/assets/js/components/Todos.vue
@@ -4,49 +4,39 @@
 
 <template>
     <div>
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Add task</h3>
+            </div>
+
+            <form role="form" action="#">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="name" class="form-control" id="name" placeholder="Enter task name here"
+                               v-model="newTodo"
+                               @keyup.enter="addTodo">
+                    </div>
+                </div>
+            </form>
+        </div>
         <div class="box">
             <div class="box-header with-border">
                 <h3 class="box-title">Tasques</h3>
-            </div>
-
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Quick Example</h3>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default">{{visibility}}</button>
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#"  v-on:click="setVisibility('all')">All</a></li>
+                        <li><a href="#"  @click="setVisibility('active')">Active</a></li>
+                        <li><a href="#/" @click="setVisibility('completed')">Completed</a></li>
+                    </ul>
                 </div>
-                <!-- /.box-header -->
-                <!-- form start -->
-                <form role="form">
-                    <div class="box-body">
-                        <div class="form-group">
-                           <input type="TEXT" class="form-control" id="name" placeholder="Nom de la tasca">
-                            @keyup.enter="addtodo"
-                        </div>
-                    </div>
-                </form>
             </div>
-
-
-
-
-
-
-
-
-
-            <div class="btn-group">
-                <button type="button" class="btn btn-default">{{Visibility}}</button>
-                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <span class="caret"></span>
-                    <span class="sr-only">Toggle Dropdown</span>
-                </button>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="#" v-on:click="setVisibiliti{'All'}">All</a></li>
-                    <li><a href="#" @click="setVisibiliti{'Active'}">Active</a></li>
-                    <li><a href="#" @click="setVisibiliti{'Completed'}">Completed</a></li>
-                </ul>
-            </div>
-
-
+            <!-- /.box-header -->
             <div class="box-body">
                 <table class="table table-bordered">
                     <thead>
@@ -60,7 +50,7 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <tr v-for="(todo, index) in todos">
+                    <tr v-for="(todo, index) in filteredTodos">
                         <td>{{index + 1}}</td>
                         <td>{{todo.name}}</td>
                         <td>{{todo.priority}}</td>
@@ -76,7 +66,9 @@
 
                 </table>
             </div>
+
             <div class="box-footer clearfix">
+
                 <ul class="pagination pagination-sm no-margin pull-right">
                     <li><a href="#">&laquo;</a></li>
                     <li><a href="#">1</a></li>
@@ -88,44 +80,33 @@
         </div>
     </div>
 </template>
-
 <script>
     export default {
         data() {
             return {
-                seen: false,
+                todos: [],
+                visibility: 'all', // 'active' 'completed'
                 newTodo: ''
-                visibility: 'all'// active completed
             }
         },
-        computed:{
-
-            filteredTodos: function(){
-
-            var filters = {
-                all: function(todos){
-                    return todos;
-                },
-
-                active: function(todos){
-                return todos.filter(function (todo){
-                    return !todo.done;
-                })
-                },
-
-                competed: function(todos){
-                    return todos.filter(function (todo){
-                    return todo.done;
-                })
-
+        computed: {
+            filteredTodos: function() {
+                var filters = {
+                    all: function (todos) {
+                        return todos;
+                    },
+                    active: function (todos) {
+                        return todos.filter(function (todo) {
+                            return !todo.done;
+                        });
+                    },
+                    completed: function (todos) {
+                        return todos.filter(function (todo) {
+                            return todo.done;
+                        });
+                    }
                 }
-
-            }
-
-            return filters[this.visibility](this.todos);
-
-
-
+                return filters[this.visibility](this.todos);
             }
         },
         created() {
@@ -133,31 +114,35 @@
             this.fetchData();
         },
         methods: {
-            newTodo: function(){
-                console.log("Fuckciona");
-               // console.log(this.newTodo);
-               // this.todo.
-
+            addTodo: function() {
+                var value = this.newTodo && this.newTodo.trim();
+                if (!value) {
+                    return;
+                }
+                this.todos.push({
+                    name: value,
+                    priority: 1,
+                    done: false
+                });
+                this.newTodo = '';
             },
-
-
-
+            setVisibility: function(visibility) {
+                this.visibility = visibility;
+            },
             reverseMessage: function() {
                 this.message = this.message.split('').reverse().join('');
             },
             fetchData: function() {
-                // GET /someUrl
+
                 this.$http.get('/api/v1/task').then((response) => {
                     console.log(response);
                     this.todos = response.data.data;
                 }, (response) => {
-                    // error callback
+
                     sweetAlert("Oops...", "Something went wrong!", "error");
                     console.log(response);
                 });
             }
-
-
         }
     }
 </script>
