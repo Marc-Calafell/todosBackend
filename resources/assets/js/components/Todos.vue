c0d599d29de363c56220dbb43ee4dd4eb1ae8440
Marc <mcalafellsmx@gmail.com>
2016-12-02 17:30:09 +0100

    paginator woks

--- resources/assets/js/components/Todos.vue
+++ resources/assets/js/components/Todos.vue
@@ -8,7 +8,8 @@
             <div class="box-header with-border">
                 <h3 class="box-title">Add task</h3>
             </div>
-
+            <!-- /.box-header -->
+            <!-- form start -->
             <form role="form" action="#">
                 <div class="box-body">
                     <div class="form-group">
@@ -36,7 +37,6 @@
                     </ul>
                 </div>
             </div>
-            <!-- /.box-header -->
             <div class="box-body">
                 <table class="table table-bordered">
                     <thead>
@@ -51,13 +51,13 @@
                     </thead>
                     <tbody>
                     <tr v-for="(todo, index) in filteredTodos">
-                        <td>{{index + from}}</td>
+                        <td>{{index + from }}</td>
                         <td>{{todo.name}}</td>
                         <td>{{todo.priority}}</td>
                         <td>{{todo.done}}</td>
                         <td>
                             <div class="progress progress-xs">
-                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                                <div class="progress-bar progress-bar-danger" style="width: 95%"></div>
                             </div>
                         </td>
                         <td><span class="badge bg-red">95%</span></td>
@@ -68,137 +68,107 @@
             </div>
 
             <div class="box-footer clearfix">
+                <span class="pull-left">Mostrant todos entre {{ from }} i {{ to }} d'un total de {{ total }} </span>
 
-
-
-                <span>mostrant tasques {{from}} - {{to}} de {{total}}</span>
+                <pagination
+                        :current-page="page"
+                        :items-per-page="perPage"
+                        :total-items="total"
+                        @page-changed="pageChanged"></pagination>
             </div>
         </div>
     </div>
 </template>
 <script>
-
-import pagination from './pagination.vue'
-
-    export default {
-        props: {
-    // Current Page
-            currentPage: {
-                type: Number,
-                required: true
-            },
-
-
-    // Visible Pages
-            visiblePages: {
-                type: Number,
-                default: 5,
-                coerce: (val) => parseInt(val),
-            }
-        },
-
-        components : {pagination},
-        data() {
-            return {
-                todos: [],
-                visibility: 'all', // 'active' 'completed'
-                newTodo: '',
-                from : 0,
-                to : 0,
-                total : 0
-            }
-        },
-        computed: {
-            filteredTodos: function() {
-                var filters = {
-                    all: function (todos) {
-                        return todos;
-                    },
-                    active: function (todos) {
-                        return todos.filter(function (todo) {
-                            return !todo.done;
-                        });
-                    },
-                    completed: function (todos) {
-                        return todos.filter(function (todo) {
-                            return todo.done;
-                        });
-                    }
-                }
-                return filters[this.visibility](this.todos);
-            }
-        },
-
-        lastPage () {
-            if (this.totalPages) {
-                   return this.totalPages
-            } else {
-                return this.totalItems % this.itemsPerPage === 0
-                ? this.totalItems / this.itemsPerPage
-                : Math.floor(this.totalItems / this.itemsPerPage) + 1
-            }
-        },
-
-        paginationRange () {
-              let start = this.currentPage - this.visiblePages / 2 <= 0
-                            ? 1 : this.currentPage + this.visiblePages / 2 > this.lastPage
-                            ? this.lowerBound(this.lastPage - this.visiblePages + 1, 1)
-                            : Math.ceil(this.currentPage - this.visiblePages / 2)
-              let range = []
-              for (let i = 0; i < this.visiblePages && i < this.lastPage; i++) {
-                range.push(start + i)
-              }
-              return range
-            }
-        },
-
-        created() {
-            console.log('Component todolist created.');
-            this.fetchData();
-        },
-        methods: {
-            addTodo: function() {
-                var value = this.newTodo && this.newTodo.trim();
-                if (!value) {
-                    return;
-                }
-                this.todos.push({
-                    name: value,
-                    priority: 1,
-                    done: false
-                });
-                this.newTodo = '';
-            },
-            setVisibility: function(visibility) {
-                this.visibility = visibility;
-            },
-            reverseMessage: function() {
-                this.message = this.message.split('').reverse().join('');
-            },
-            fetchData: function() {
-
-                this.$http.get('/api/v1/task').then((response) => {
-                    console.log(response);
-                    this.todos = response.data.data;
-                }, (response) => {
-
-                    sweetAlert("Oops...", "Something went wrong!", "error");
-                    console.log(response);
-                });
-            },
-            fetchPage: function(page) {
-
-                this.$http.get('/api/v1/task? page=' + page ).then((response) => {
-                        console.log(response);
-                        this.todos = response.data.data;
-                        this.perPage = response.data.per_page;
-                        this.from = response.data.from;
-                        this.to = response.data.to;
-                        this.total = response.data.total;
+import Pagination from './pagination.vue'
+export default {
+    components : { Pagination },
+    data() {
+        return {
+            todos: [],
+            visibility: 'all', // 'active' 'completed'
+            newTodo: '',
+            perPage: 5,
+            from: 0,
+            to: 0,
+            total: 0,
+            page: 1
+        }
+    },
+    computed: {
+        filteredTodos: function() {
+            var filters = {
+                all: function (todos) {
+                    return todos;
+                },
+                active: function (todos) {
+                    return todos.filter(function (todo) {
+                        return !todo.done;
+                    });
+                },
+                completed: function (todos) {
+                    return todos.filter(function (todo) {
+                        return todo.done;
+                    });
                 }
             }
+            return filters[this.visibility](this.todos);
+        }
+    },
+    created() {
+        console.log('Component todolist created.');
+        this.fetchData();
+    },
+    methods: {
+        pageChanged: function(pageNum) {
+            this.page = pageNum;
+            this.fetchPage(pageNum);
+        },
+        addTodo: function() {
+            var value = this.newTodo && this.newTodo.trim();
+            if (!value) {
+                return;
+            }
+            this.todos.push({
+                name: value,
+                priority: 1,
+                done: false
+            });
+            this.newTodo = '';
+        },
+        setVisibility: function(visibility) {
+            this.visibility = visibility;
+        },
+        fetchData: function() {
+            return this.fetchPage(1);
+        },
+        fetchPage: function(page) {
+            this.$http.get('/api/v1/task?page=' + page).then((response) => {
+                console.log(response);
+                this.todos = response.data.data;
+                this.perPage = response.data.per_page;
+                this.to = response.data.to;
+                this.from = response.data.from;
+                this.total = response.data.total;
+            }, (response) => {
+                sweetAlert("Oops...", "Something went wrong!", "error");
+                console.log(response);
+            });
+        },
 
-
+        addTodoApi: function(todo) {
+            this.$http.get('/api/v1/task).then((response) => {
+                console.log(response);
+                this.todos = response.data.data;
+                this.perPage = response.data.per_page;
+                this.to = response.data.to;
+                this.from = response.data.from;
+                this.total = response.data.total;
+            }, (response) => {
+                sweetAlert("Oops...", "Something went wrong!", "error");
+                console.log(response);
+            });
         }
     }
+}
 </script>
