ae548108043b726d67c536c53a05497bee71183f
Marc <mcalafellsmx@gmail.com>
2016-12-13 19:24:02 +0100

    Securized API

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,9 @@ use Illuminate\Http\Request;
 //})->middleware('auth:api');
 //, 'middleware' => 'auth:api']
 
-Route::group(['prefix' => 'v1'], function () {
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function () {
     Route::resource('task', 'TasksController');
     Route::resource('user', 'UsersController');
     Route::resource('user.task', 'UserTasksController');
 });
+
