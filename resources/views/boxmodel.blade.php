e6e047728b924087ec5b7736dd8f1da9d059e579
Marc <mcalafellsmx@gmail.com>
2017-01-17 15:54:01 +0100

    Sync

--- resources/views/boxmodel.blade.php
+++ resources/views/boxmodel.blade.php
@@ -1,105 +1,142 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
 </head>
-
-
-
 <body>
 
 <style>
 
-	*{
-		-webkit-box-align: ;
-		moz
-
-	}
-	div#header {
-		position: static;
-		width: 100%;
-		height: 90px;
-		/*margin: 20px auto;*/
-		border: solid pink 2px;
-
-		color: black;
-		/*margin: auto;*/
-		/*box-sizing: border-box;*/
+	body {
+		margin: 0px;
+		margin-bottom: 50px;
 	}
 
-	div#sidebar {
-		color: red;
+	#nav {
+		position: absolute;
+		left:0px;
+		width: 200px;
+		border: solid black 1px;
+		background-color: lightgrey;
+	}
+
+	#section {
+		margin-left: 200px;
+		padding: 10px;
+		background-color: lightcoral;
+	}
+
+	#footer {
 		position: fixed;
-		left: 0;
-		width: 150px;
-		height: 100%;
-		margin: auto;
-		padding: 25px;
-		border: solid cyan 2px;
-		box-sizing: border-box;
-
+		bottom: 0px;
+		height: 50px;
+		border: solid black 1px;
+		width: 100%;
+		background-color: lightblue;
 	}
 
-	div#content {
-		color: blue;
-		position: fixed;
-		right: 0;
-		width: 100%;
-		margin: auto;
-		padding: 25px;
-		border: solid cyan 2px;
-		box-sizing: border-box;
-
-	}
-
-	div#footer {
-		position: static;
-		bottom: auto;
-		width: 100%;
-		height: 90px;
-		/*margin: 20px auto;*/
-		border: solid pink 2px;
-
-		color: black;
-		/*margin: auto;*/
-		/*box-sizing: border-box;*/
+	#container {
+		position:relative
 	}
 
 </style>
 
 <div id="container">
-<div id="header">
-	<h1>Header here</h1>
+	<div id="nav">
+		<ul>
+			<li>link1</li>
+			<li>Link2</li>
+			<li>Link3</li>
+			<li>Contact</li>
+			<li>About</li>
+		</ul>
+	</div>
 
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cumque dolor est eveniet incidunt magnam modi neque nesciunt perspiciatis porro, quibusdam quisquam recusandae sapiente sint tenetur velit voluptates. Explicabo, fugiat.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum quaerat quia. Beatae nisi quasi sequi! Adipisci iusto non omnis sint! Amet autem dolores error nihil nostrum officiis repellendus saepe.
+	</div>
+
+	<div id="footer">
+		@copyright Acacha Inc. asdas dasd asd
+	</div>
 </div>
 
-<div id="sidebar">
-	<ul>
-		<li>Link A</li>
-		<li>Link B</li>
-		<li>Link C</li>
-		<li>Link D</li>
-		<li>Link E</li>
-	</ul>
 
-</div>
-
-<div id="content">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aut blanditiis ducimus eius eos esse harum non, provident quia voluptates. Accusamus animi corporis dolor exercitationem hic iure, non officia rem rerum tempora totam velit. Ad asperiores cum cupiditate deserunt esse illum itaque, laboriosam nam, necessitatibus nemo odio reiciendis rem totam! Ab amet consequatur deserunt eos explicabo fugiat id laudantium, magni obcaecati porro, quia quibusdam quod recusandae rem, tempora. Aspernatur aut cumque doloremque dolores enim maiores nemo quod voluptas? Aliquid debitis earum eveniet fuga harum impedit inventore quis similique. Deleniti expedita nam nemo rerum vero? Culpa delectus distinctio doloremque fuga necessitatibus neque obcaecati quia ratione reiciendis vitae! Animi autem dolore ex exercitationem facere incidunt laudantium magnam magni pariatur quaerat. Delectus earum minus nisi saepe? Animi aperiam architecto autem distinctio doloribus dolorum earum eligendi eos eveniet exercitationem fugiat harum impedit ipsa iusto, maiores nesciunt non officiis perferendis, perspiciatis possimus quos repellendus rerum sequi similique soluta voluptas voluptatibus. Eveniet fuga possimus reiciendis sint. Architecto assumenda distinctio numquam pariatur qui. Aliquid asperiores at atque blanditiis, commodi corporis deleniti dolor doloremque dolores dolorum earum eos eveniet excepturi facere fugit inventore ipsam itaque iure labore molestias mollitia neque odio perferendis quae reprehenderit, rerum tempora veniam voluptatum?
-</div>
-
-<div id="footer">
-	<strong>Footer here</strong>
-
-</div>
-
-</div>
 
 </body>
 </html>
